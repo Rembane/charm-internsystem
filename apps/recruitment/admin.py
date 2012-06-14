9b0771902c4d273c94322c4a0bfa49250e77a6a5
Rembane <andeke@gmail.com>
2012-06-14 17:34:59 +0200

    Added all models to the admin interface.

--- apps/recruitment/admin.py
+++ apps/recruitment/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from recruitment.models import Person
+from recruitment.models import DriverLicense, ForkLiftLicense, Language, Person, ShirtSize, StudyArea
 
+admin.site.register(DriverLicense)
+admin.site.register(ForkLiftLicense)
+admin.site.register(Language)
 admin.site.register(Person)
+admin.site.register(ShirtSize)
+admin.site.register(StudyArea)
 
